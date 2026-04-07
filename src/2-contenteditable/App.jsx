a7a4666d88691c1ee406d594fe83660d53eec458
Zsolt Ero <zsolt.ero@gmail.com>
2026-04-07 13:10:37 +0200

    work

--- src/2-contenteditable/App.jsx
+++ src/2-contenteditable/App.jsx
@@ -1,11 +1,33 @@
+import { baseKeymap } from 'prosemirror-commands'
+import { keymap } from 'prosemirror-keymap'
+import { Schema } from 'prosemirror-model'
+import { EditorState } from 'prosemirror-state'
+import { EditorView } from 'prosemirror-view'
+import { onMount } from 'solid-js'
+
 import './App.css'
+import 'prosemirror-view/style/prosemirror.css'
 
 function App() {
-  return (
-    <div class="contentEditable" contentEditable={true} ref={ref}>
-      solid
-    </div>
-  )
+  let ref
+
+  onMount(() => {
+    console.log('onMount')
+    const editor = new EditorView(ref, {
+      state: EditorState.create({
+        schema: new Schema({
+          nodes: {
+            doc: { content: 'paragraph+' },
+            paragraph: { content: 'text*', toDOM: () => ['p', 0] },
+            text: { group: 'inline' },
+          },
+        }),
+        plugins: [keymap(baseKeymap)],
+      }),
+    })
+  })
+
+  return <div class="ce prosemirror-parent" ref={ref} />
 }
 
 export default App
